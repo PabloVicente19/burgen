33497c6e7dbe1cee2d3407a83434c5d1f90faf16
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-23 19:25:55 -0300

    arregle las cards

--- src/js/app.js
+++ src/js/app.js
@@ -2,14 +2,14 @@
 const createCardBurguer = (product) => {
 	const { id, name, image, price, group, ingredients } = product;
 	return `
-  <div class="card " style="width: 16rem">
+  <div class="card d-flex justify-content-between" style="width: 16rem">
       <div class="img-container d-block">
         <img src=${image} class="img-fluid w-100 h-100 " alt=${name}>
       </div>
-    <div class="card-body d-flex flex-column justify-content-center">
+    <div class="card-body d-flex flex-column justify-content-between">
       <h5 class="card-title text-center">${name}</h5>
       <p class="lh-1 card-text text-center w-100">${ingredients}</p>
-      <button href="#" class="btn btn-primary">Agregar</button>
+      <button href="#" class="btn btn-primary align-self-center pt-2 pb-2 ps-4 pe-4">Agregar</button>
     </div>
   </div>`;
 };
