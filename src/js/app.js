fafd78befde96937225323502d5ee664553b4ed0
Pablo Vicente <pablovicente19@hotmail.com>
2023-06-24 21:52:11 -0300

    trabajar en la logica del carrito

--- src/js/app.js
+++ src/js/app.js
@@ -1,7 +1,6 @@
 const copyOfProducts = [...products];
-// Filto los productos por categoria
-// let filterByBurger = copyOfProducts.filter(product => product.category == 'burger')
- 
+let cart = [];
+
 // funcion que  crea las cards de las hamburguesas
 const createCardBurguer = (product) => {
 	const { id, name, image, price, group, ingredients } = product;
@@ -13,16 +12,15 @@ const createCardBurguer = (product) => {
         </div>
       <div class="card-body d-flex flex-column justify-content-between">
         <h5 class="card-title text-center">${name}</h5>
-        <p class="lh-1 card-text text-center w-100">${ingredients.join("")}</p>
-        <button href="#" class="btn btn-primary align-self-center pt-2 pb-2 ps-4 pe-4">Agregar</button>
+        <p class="lh-1 card-text text-center w-100">${ingredients.join(", ")}</p>
+        <p class="text-center mb-2"> $: ${price} </p>
+        <button href="#" class="btn btn-primary align-self-center pt-2 pb-2 ps-4 pe-4" data-id=${id}>Agregar</button>
       </div>
     </div>`;
   }
 };
 
-
-
-
+// Funcion que crea las card de las bebidas
 const createBeerCard = (product) => {
 	const { id, name, image, price, group, ingredients } = product;
   if(product.category == 'beer'){ 
@@ -32,11 +30,21 @@ return`<div class="card d-flex justify-content-between" style="width: 16rem">
           </div>
         <div class="card-body d-flex flex-column justify-content-between">
           <h5 class="card-title text-center">${name}</h5>
-          <p class="lh-1 card-text text-center w-100">${ingredients}</p>
-          <button href="#" class="btn btn-primary align-self-center pt-2 pb-2 ps-4 pe-4">Agregar</button>
+          <p class="lh-1 card-text text-center w-100" mb-2>$: ${price}</p>
+          <button href="#" class="btn btn-primary align-self-center pt-2 pb-2 ps-4 pe-4" data-id=${id}>Agregar</button>
         </div>
       </div>`;
 }}
+// Funcion que sirve para renderizar
+// Category sirve para separar dentro del objeto la categoria
 const renderCard = (container, arr, category) => {
 	return (container.innerHTML += arr.map(category).join(""));
 };
+
+const addProductCart = (e) => {
+  // Almaceno el id en la variable
+  let productId = e.target.dataset.id
+  // Si no existe el id que retorne
+  if(!productId) return;
+
+}
--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,6 @@
 const $menuIcon = document.querySelector(".menu-bars");
 const $menuList = document.querySelector(".navbar-list");
+const $mainContainer = document.querySelector('.main-container')
 const $menuBurger = document.querySelector(".menu-burgers-container");
 const $menuBeer = document.querySelector('.menu-beer-container');
 
@@ -11,3 +12,5 @@ $menuIcon.addEventListener("click", () => {
 renderCard($menuBurger,copyOfProducts,createCardBurguer);
 // Renderizo las cards de las cervezas
 renderCard($menuBeer,copyOfProducts,createBeerCard);
+
+$mainContainer.addEventListener('click',addProductCart)
